66c358deac701a2ddaa1883bfd85dac21fb53bb8
GitHub Action <action@github.com>
2023-06-16 07:21:26 +0100

    Brew formula update for ghat version v0.0.8

--- ghat.rb
+++ ghat.rb
@@ -5,20 +5,20 @@
 class Ghat < Formula
   desc "Ghat is a tool for updating GHA dependencies"
   homepage "https://github.com/JamesWoolfenden/ghat#readme"
-  version "0.0.7"
+  version "0.0.8"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.7/ghat_0.0.7_darwin_amd64.tar.gz"
-      sha256 "c30d1670ccc9ee950385b88d7cb50af588cb5b03ba1a8aa1ee1505020ba7b5b7"
+    if Hardware::CPU.arm?
+      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.8/ghat_0.0.8_darwin_arm64.tar.gz"
+      sha256 "dc35ee747806c372562f8612d8420a727cd730a1ed0bc0670aa61b81b20c8f59"
 
       def install
         bin.install "ghat"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.7/ghat_0.0.7_darwin_arm64.tar.gz"
-      sha256 "338a29b3680bdef2e9ff8d085eeaab25f41822dbfc00982fbb807ffae3e4538d"
+    if Hardware::CPU.intel?
+      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.8/ghat_0.0.8_darwin_amd64.tar.gz"
+      sha256 "ca2d97b0fc2476122e76a73b8628dd1b870622e59a7970def941a1b360e49f6e"
 
       def install
         bin.install "ghat"
@@ -28,16 +28,16 @@ class Ghat < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.7/ghat_0.0.7_linux_amd64.tar.gz"
-      sha256 "8f9de940a7dd43d824c94d9a1431323e6033f714a5459a34698927750f736f88"
+      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.8/ghat_0.0.8_linux_amd64.tar.gz"
+      sha256 "c10f3f1c9d6cc72954593d3ee29ce90826e0c3c2ef06afdc15ab45941b3f87ed"
 
       def install
         bin.install "ghat"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.7/ghat_0.0.7_linux_arm64.tar.gz"
-      sha256 "7d3ccef09e11fca279ff0d22e42f66699587268c109585bcb8be0297b0b1eca3"
+      url "https://github.com/JamesWoolfenden/ghat/releases/download/v0.0.8/ghat_0.0.8_linux_arm64.tar.gz"
+      sha256 "afa54b6f8c5c91cd6658a74e26fef6a911f4b97f1568de5cd83e696b33c1ca80"
 
       def install
         bin.install "ghat"
